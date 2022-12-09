9002de430246ea4312656270347d12bd6fe29a98
Torrente <71377802+BalintKollar852@users.noreply.github.com>
2022-12-09 13:11:56 +0100

    asd

--- scripts/Car.cs
+++ scripts/Car.cs
@@ -3,19 +3,18 @@ using System;
 
 public class Car : RigidBody2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
-    // Called when the node enters the scene tree for the first time.
+    RigidBody2D wheel1;
+    RigidBody2D wheel2;
     public override void _Ready()
     {
-        
+        wheel1 = GetNode("WheelHolder/Wheel") as RigidBody2D;
+        wheel2 = GetNode("WheelHolder2/Wheel") as RigidBody2D;
     }
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    public override void _PhysicsProcess(float delta)
+    {
+        if(Input.IsActionPressed("forward")){
+            wheel1.ApplyTorqueImpulse(delta * 1000 * 60);
+            wheel2.ApplyTorqueImpulse(delta * 1000 * 60);
+        }
+    }
 }
