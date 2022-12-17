c8457ec03adf9a40a15d429f5a525a56b405f5be
BalintKollar852 <balintkollar852@gmail.com>
2022-12-17 20:52:09 +0100

    asd

--- scripts/Car.cs
+++ scripts/Car.cs
@@ -5,26 +5,41 @@ public class Car : RigidBody2D
 {
 	RigidBody2D wheel1;
 	RigidBody2D wheel2;
+	private int max_speed = 90;
+	private Vector2 checkPoint;
 	public override void _Ready()
 	{
 		wheel1 = GetNode("WheelHolder/Wheel") as RigidBody2D;
 		wheel2 = GetNode("WheelHolder2/Wheel") as RigidBody2D;
+		wheel1.ContactMonitor = true;
+		wheel1.ContactsReported = 1;
+		wheel2.ContactMonitor = true;
+		wheel2.ContactsReported = 1;	
+		ContactMonitor = true;
+		ContactsReported = 1;
 	}
 	public override void _PhysicsProcess(float delta)
 	{
 		if(Input.IsActionPressed("forward")){
-			wheel1.ApplyTorqueImpulse(delta * 60000 * 60);
-			wheel2.ApplyTorqueImpulse(delta * 60000 * 60);
+			if(wheel1.AngularVelocity < max_speed || wheel2.AngularVelocity < max_speed){
+			wheel1.ApplyTorqueImpulse(delta * 10000);
+			wheel2.ApplyTorqueImpulse(delta * 10000);	
+			}
 		}
 		if(Input.IsActionPressed("ui_right")){
-			RotationDegrees += 0.5f; 
+			if(wheel1.GetCollidingBodies().Count == 0 && wheel2.GetCollidingBodies().Count == 0 && GetCollidingBodies().Count == 0){
+				RotationDegrees += 1; 
+			}
 		}
 		if(Input.IsActionPressed("ui_left")){
-			RotationDegrees -= 0.5f; 
+			if(wheel1.GetCollidingBodies().Count == 0 && wheel2.GetCollidingBodies().Count == 0 && GetCollidingBodies().Count == 0){
+				RotationDegrees -= 1; 
+			}
 		}
 		if(Input.IsActionPressed("ui_down")){
-			wheel2.ApplyTorqueImpulse(delta * -60000 * 60); 
-			
+			if(wheel1.AngularVelocity < -max_speed || wheel2.AngularVelocity < -max_speed){
+			wheel2.ApplyTorqueImpulse(delta * -10000); 
+			}
 		}
 	}
 }
