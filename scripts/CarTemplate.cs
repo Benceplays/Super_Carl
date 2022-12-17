1f16dfffb3d65d549f9d5725d9dc6e5ed2bb6b84
Milaan1337 <71323999+Milaan1337@users.noreply.github.com>
2022-12-17 20:06:55 +0100

    Garage specification and base structure

---
+++ scripts/CarTemplate.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public class CarTemplate : Node2D
+{
+    //tipusok:(1 = basecar; 2 = busz)
+    public int type;
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        GD.Print(type);
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
--- scripts/Menu.cs
+++ scripts/Menu.cs
@@ -1,12 +1,15 @@
 using Godot;
 using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using File = System.IO.File;
+using System.IO;
 
 public class Menu : Node2D
 {
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
-
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
