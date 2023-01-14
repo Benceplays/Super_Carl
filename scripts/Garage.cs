da8dac0acb554c7be927a1b0431c47f2c8ed1346
Milaan1337 <71323999+Milaan1337@users.noreply.github.com>
2023-01-14 17:58:04 +0100

    garágae

--- scripts/Garage.cs
+++ scripts/Garage.cs
@@ -7,82 +7,153 @@ using Newtonsoft.Json;
 
 public class Garage : Node2D
 {
-    public string tuningjson;
-    public string player_json;
-    public Label money_label;
-    public Label days_label;
-    public Sprite car0;
-    public Sprite car1;
-    public Sprite car2;
-    public Sprite[] car_sprites;
-    public override void _Ready()
-    {
-        GetTree().Paused = false;
-        tuningjson = File.ReadAllText(@"scripts/Tunings.json");
-        if (tuningjson == "")
-        {
-            WritePlayerData(true);
-        }
-        player_json = File.ReadAllText(@"scripts/Player.json");
-        money_label = (Label)GetNode("top_Hud/money");
-        days_label = (Label)GetNode("top_Hud/days");
-        var get_datas = JsonConvert.DeserializeObject<ConfigBody>(player_json);
-        money_label.Text = get_datas.money + "$";
-        days_label.Text = "Days " + get_datas.Days;
+	public string tuningjson;
+	public string player_json;
+	public Label money_label;
+	public Label days_label;
+	public Sprite car0;
+	public Sprite car1;
+	public Sprite car2;
+	public Sprite[] car_sprites;
+	public int current_car;
+	public int current_view_car;
+	public Button left_arrow;
+	public Button right_arrow;
+	public override void _Ready()
+	{
+		GetTree().Paused = false;
+		tuningjson = File.ReadAllText(@"scripts/Tunings.json");
+		if (tuningjson == "")
+		{
+			WritePlayerData(true);
+		}
+		player_json = File.ReadAllText(@"scripts/Player.json");
+		money_label = (Label)GetNode("top_Hud/money");
+		days_label = (Label)GetNode("top_Hud/days");
+		var get_datas = JsonConvert.DeserializeObject<ConfigBody>(player_json);
+		money_label.Text = get_datas.money + "$";
+		days_label.Text = "Days " + get_datas.Days;
 
-        car0 = (Sprite)GetNode("mid_hud/car0");
-        car1 = (Sprite)GetNode("mid_hud/car1");
-        car2 = (Sprite)GetNode("mid_hud/car2");
+		left_arrow = (Button)GetNode("left_arrow");
+		right_arrow = (Button)GetNode("right_arrow");
 
-        car_sprites = new Sprite[3];
-        car_sprites[0] = car0;
-        car_sprites[1] = car1;
-        car_sprites[2] = car2;
-    }
 
-    public void _on_Next_pressed(){
-        GetTree().ChangeScene("res://scenes/Game.tscn");
-    }
+		car0 = (Sprite)GetNode("mid_hud/car0");
+		car1 = (Sprite)GetNode("mid_hud/car1");
+		car2 = (Sprite)GetNode("mid_hud/car2");
 
-    public void _on_Back_pressed()
-    {
-        for (int i = 0; i < car_sprites.Length; i++)
-        {
-            car_sprites[i].SetPosition(new Vector2(car_sprites[i].Position.x - 2000, car_sprites[i].Position.y));
-            GD.Print(i + " kocsi:" + car_sprites[i].Position.x);
-        }
-    }
+		car_sprites = new Sprite[3];
+		car_sprites[0] = car0;
+		car_sprites[1] = car1;
+		car_sprites[2] = car2;
+		
+		current_car = get_datas.currentcar; //jelenlegi kocsi lekérdezés
+		current_view_car = current_car;
+		checkArrows();
 
-    public void WritePlayerData(bool loadDefault)
-    {
-        string text = File.ReadAllText(@"scripts/Player.json");
-        var get_datas = JsonConvert.DeserializeObject<ConfigBody>(text);
-        
-        string text2 = File.ReadAllText(@"scripts/Tunings.json");
-        var get_tunings = JsonConvert.DeserializeObject<ConfigBody>(text2);
-        
-        //Amennyiben ures a json fájl =>
-        if (loadDefault)
-        {
-            int index = -1;
-            foreach (var car in get_datas.Cars)
-            {
-                index++;
-                Dictionary<string, int> kocsi = new Dictionary<string, int>();
-                kocsi.Add("id",car);
-                kocsi.Add("engine",0);
-                kocsi.Add("gun",0);
-                kocsi.Add("petrol",0);
-                kocsi.Add("wheel", 0);
-                if (index != 0)
-                {
-                    File.AppendAllText(@"scripts/Tunings.json","\n" + JsonConvert.SerializeObject(kocsi));
-                }
-                else
-                {
-                    File.AppendAllText(@"scripts/Tunings.json", JsonConvert.SerializeObject(kocsi));
-                }
-            }
-        }
-    }
+		//Jelenlegi autó berakása középre
+		for (int i = 0; i < car_sprites.Length; i++)
+		{
+			car_sprites[i].SetPosition(new Vector2((car_sprites[i].Position.x - current_car*2000), car_sprites[i].Position.y));
+		}
+	}
+
+	public override void _Process(float delta)
+	{
+	}
+	
+	
+	public void _on_left_arrow_pressed()
+	{
+		//Tween tween = new Tween();  ezzel lehet majd megcsinálni,hogy animáltan mozogjon ami azért valljuk be elég menő lenne! Ötletgazda:Korall, megvalósító:Korall
+		right_arrow.Visible = true;
+		for (int i = 0; i < car_sprites.Length; i++)
+		{
+			car_sprites[i].SetPosition(new Vector2((car_sprites[i].Position.x + 2000), car_sprites[i].Position.y));
+		}
+		current_view_car -= 1;
+		checkArrows();
+
+	}
+	
+	public void _on_right_arrow_pressed()
+	{
+		left_arrow.Visible = true;
+		for (int i = 0; i < car_sprites.Length; i++)
+		{
+			car_sprites[i].SetPosition(new Vector2((car_sprites[i].Position.x - 2000), car_sprites[i].Position.y));
+		}
+		current_view_car += 1;
+		checkArrows();
+
+	}
+
+	public void checkArrows()
+	{
+		if (current_view_car == 0)
+		{
+			left_arrow.Visible = false;
+		}else if (current_view_car == 2)
+		{
+			GD.PrintRaw("gecikolar");
+			right_arrow.Visible = false;
+		}
+	}
+	public void _on_HSlider_value_changed(float value)
+{
+	//Kocsik közötti görgetés
+	//1012 => mert 506ot kivonunk belőle, hogy ne a képernyő bal oldalán legyen hanem középen
+	for (int i = 0; i < car_sprites.Length; i++)
+	{
+		car_sprites[i].SetPosition(new Vector2((i*2000) + 1012 - value, car_sprites[i].Position.y));
+		if (car_sprites[i].Position.x == 506) //ha ez a középen lévő autó akkor legyen ez a current
+		{
+			//todo megnézni birtokolja e a járművet
+			//todo elmenteni json-ba a currentcart és azt lekérdezni nem pedig i értéket adni neki.
+			current_car = i;
+		}
+	}
+}
+
+	public void _on_Next_pressed(){
+		GetTree().ChangeScene("res://scenes/Game.tscn");
+	}
+
+	public void _on_Back_pressed()
+	{
+		
+	}
+
+	public void WritePlayerData(bool loadDefault)
+	{
+		string text = File.ReadAllText(@"scripts/Player.json");
+		var get_datas = JsonConvert.DeserializeObject<ConfigBody>(text);
+		
+		string text2 = File.ReadAllText(@"scripts/Tunings.json");
+		var get_tunings = JsonConvert.DeserializeObject<ConfigBody>(text2);
+		
+		//Amennyiben ures a json fájl =>
+		if (loadDefault)
+		{
+			int index = -1;
+			foreach (var car in get_datas.Cars)
+			{
+				index++;
+				Dictionary<string, int> kocsi = new Dictionary<string, int>();
+				kocsi.Add("id",car);
+				kocsi.Add("engine",0);
+				kocsi.Add("gun",0);
+				kocsi.Add("petrol",0);
+				kocsi.Add("wheel", 0);
+				if (index != 0)
+				{
+					File.AppendAllText(@"scripts/Tunings.json","\n" + JsonConvert.SerializeObject(kocsi));
+				}
+				else
+				{
+					File.AppendAllText(@"scripts/Tunings.json", JsonConvert.SerializeObject(kocsi));
+				}
+			}
+		}
+	}
 }
--- scripts/Menu.cs
+++ scripts/Menu.cs
@@ -8,43 +8,43 @@ using System.IO;
 public class Menu : Node2D
 {
 
-    public override void _Ready()
-    {
-        string text = File.ReadAllText(@"scripts/Options.json");
-        var get_options = JsonConvert.DeserializeObject<ConfigBody>(text);
-        // set the window mode
-        switch (get_options.displaymode)
-        {
-            case 0:
-                OS.WindowResizable = true;
-                OS.WindowMaximized = false;
-                OS.WindowFullscreen = false;
-                break;
-            case 1:
-                OS.WindowFullscreen = true;
-                OS.WindowMaximized = false;
-                OS.WindowBorderless = false;
-                break;
-            case 2:
-                OS.WindowMaximized = true;
-                OS.WindowFullscreen = false;
-                OS.WindowBorderless = false;
-                break;
+	public override void _Ready()
+	{
+		string text = File.ReadAllText(@"scripts/Options.json");
+		var get_options = JsonConvert.DeserializeObject<ConfigBody>(text);
+		// set the window mode
+		switch (get_options.displaymode)
+		{
+			case 0:
+				OS.WindowResizable = true;
+				OS.WindowMaximized = false;
+				OS.WindowFullscreen = false;
+				break;
+			case 1:
+				OS.WindowFullscreen = true;
+				OS.WindowMaximized = false;
+				OS.WindowBorderless = false;
+				break;
+			case 2:
+				OS.WindowMaximized = true;
+				OS.WindowFullscreen = false;
+				OS.WindowBorderless = false;
+				break;
 
-        }
-    }
-    public void _on_Play_pressed()
-    {
-        GetTree().ChangeScene("res://scenes/Garage.tscn");
-    }
-    public void _on_Options_pressed() 
-    { 
-        GetTree().ChangeScene("res://scenes/Options.tscn");
-    }
-    public void _on_Exit_pressed()
-    {
-        GetTree().Quit();
-    }
+		}
+	}
+	public void _on_Play_pressed()
+	{
+		GetTree().ChangeScene("res://scenes/Garage.tscn");
+	}
+	public void _on_Options_pressed() 
+	{ 
+		GetTree().ChangeScene("res://scenes/Options.tscn");
+	}
+	public void _on_Exit_pressed()
+	{
+		GetTree().Quit();
+	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
