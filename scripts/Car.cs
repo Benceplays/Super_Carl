4ff0614984d6754b4cb92e5876af7b476106f6cf
Benceplays <nemeth.csaba.bence@gmail.com>
2023-02-01 08:54:50 +0100

    par cucc

--- scripts/Car.cs
+++ scripts/Car.cs
@@ -138,7 +138,7 @@ public class Car : RigidBody2D
 		}
 		petrolprogress = GetNode("HUD/Petrol") as TextureProgress;
         petrolprogress.Value = gas;
-		GD.Print(riptimer);
+		//GD.Print(riptimer);
 		if (this.LinearVelocity.x < 25)
 		{
     		riptimer -= delta;
