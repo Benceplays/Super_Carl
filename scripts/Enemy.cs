7df7a852358150f851747e0bb23567447300b59f
Benceplays <nemeth.csaba.bence@gmail.com>
2023-02-02 09:09:58 +0100

    par cucc

--- scripts/Enemy.cs
+++ scripts/Enemy.cs
@@ -5,9 +5,16 @@ public class Enemy : KinematicBody2D
 {
 	private KinematicBody2D enemy;
 	private float deltaplus;
+	private int force = 100000;
+	private Timer timer;
+	private Area2D ZombieHitbox;
+	private CollisionPolygon2D zombiecollision;
 	public override void _Ready()
 	{
 		//enemy = GetNode("Enemy") as KinematicBody2D;
+		timer = GetNode("ZombieNyek") as Timer;
+		zombiecollision = GetNode("CollisionPolygon2D") as CollisionPolygon2D;
+		ZombieHitbox = GetNode("ZombieHitbox") as Area2D;
 	}
 	public override void _Process(float delta)
 	{
@@ -23,10 +30,17 @@ public class Enemy : KinematicBody2D
 	private void _on_ZombieHitbox_body_entered(KinematicBody2D body)
 	{
 		if (body.IsInGroup("car")){
-		GD.Print("idiota milan lefut");
-		Vector2 velocity = new Vector2(50000000, 0);
-		MoveAndSlide(velocity * deltaplus);
+			timer.Start();
+			this.Rotate(1);
+			zombiecollision.Disabled = false;
+			ZombieHitbox.Monitoring = false;
+			Vector2 velocity = new Vector2(800, 0);
+			MoveAndSlide(velocity * force * deltaplus);
 		}
+	}
+	public void _on_ZombieNyek_timeout()
+	{
+		QueueFree();
 	}
 
 }
